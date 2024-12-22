132d49a65d2f3fb601b039b30d6dc9a65b335ab3
vikash <vikashkelly@gmail.com>
2024-12-22 10:17:16 +0530

    update

--- Chapter 4/05_incerement_decrement.c
+++ Chapter 4/05_incerement_decrement.c
@@ -5,9 +5,11 @@ int main(){
     i = i+5; // = 10
     printf("Now i = %d\n",i);
     printf("The value of i is %d\n",++i);
-    printf("pre increment = %d",i);
+    printf("pre increment = %d\n",i);
 
-/* i++ print i first and then increments i (post increment operator)
-++i increment first and then prints i (post increment operator) */
+    i +=2;   // same as i = i+2
+    printf("Now i = %d\n", i);
+    /* i++ print i first and then increments i (post increment operator)
+    ++i increment first and then prints i (post increment operator) */
     return 0;
 }
