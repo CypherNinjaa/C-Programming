26cd56b1404c2a5666ace09b85612bb882e1f3e1
vikash <vikashkelly@gmail.com>
2024-12-25 14:07:11 +0530

    updated

--- Chapter 8/05_string_function.c
+++ Chapter 8/05_string_function.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+void pointer(char *);
 int main()
 {
     char st[30];
     gets(st);
+    pointer(st);
     printf("%d", strlen(st));
     return 0;
+}
+void pointer(char *ptr)
+{
+    printf("%s", ptr);
 }
