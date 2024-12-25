c81300983f4c6a7ebd096a238335f9d2b46c66c9
vikash <vikashkelly@gmail.com>
2024-12-25 12:11:29 +0530

    printing st

---
+++ Chapter 8/02_printing_strings.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    // char st[] = {'a', 'b', 'c', '\0'};
+    char st[] = "vikash"; // same as above code
+
+    // printing by loop
+
+    /* for (int i = 0; i < 6; i++)
+     {
+         printf("%c", st[i]);
+     }*/
+
+    // printing directly
+
+    printf("%s", st);
+
+    return 0;
+}
