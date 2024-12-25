554e990bc46f1785216404d5b203467c34cca93c
vikash <vikashkelly@gmail.com>
2024-12-25 11:43:00 +0530

    problem

--- Chapter 7 practice set/06_problem_6.c
+++ Chapter 7 practice set/06_problem_6.c
@@ -22,12 +22,13 @@ int main()
             arr[i][j] = mul[i] * (j + 1);
         }
     }
+
     for (int i = 0; i < n; i++)
     {
 
         for (int j = 0; j < 10; j++)
         {
-            printf("the valu of arr[%d][%d] is %d\n", i, j, arr[i][j]);
+            printf("%d x %d = %d\n", mul[i], j + 1, arr[i][j]);
         }
         printf("\n");
     }
