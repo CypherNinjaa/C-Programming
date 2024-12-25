48a5bddc96bf61d78fe12747d63b8cad8ccac024
vikash <vikashkelly@gmail.com>
2024-12-25 14:23:17 +0530

    problem

---
+++ Chapter 8 practice set/05_problem.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[] = "mera pizza ka last slice ghar pe hai";
+    for (int i = 0; i < strlen(str); i++)
+    {
+        str[i] = str[i] + 1;
+    }
+    printf("%s", str);
+    return 0;
+}
