ebadc6750e9cff9f1e052b753ffe44a48e5fdfc9
vikash <vikashkelly@gmail.com>
2024-12-25 12:04:31 +0530

    st

---
+++ Chapter 8/01_string_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    // char st[] = {'a', 'b', 'c', '\0'};
+    char st[] = "vikash"; // same as above code
+    for (int i = 0; i < 6; i++)
+    {
+        printf(" character is %c \n", st[i]);
+    }
+
+    return 0;
+}
