c4a43d7fd0d869d8fac687be71cd3516c223bfc8
vikash <vikashkelly@gmail.com>
2024-12-25 13:47:25 +0530

    string

---
+++ Chapter 8/04_gets.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    char st[30];
+    gets(st); // the entered string is stored in st
+    // printf("%s", st);
+    puts(st);
+    printf("Vikash");
+    return 0;
+}
