5b431c87d391f7a5e98ce5093cccd53198aa008f
vikash <vikashkelly@gmail.com>
2024-12-26 14:13:18 +0530

    problem 6

--- Chapter 9 practice set/06_problem.c
+++ Chapter 9 practice set/06_problem.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
 #include <string.h>
-typedef struct banckacc
+
+typedef struct bankacc
 {
     int accNo;
     char name[34];
     char ifsc[12];
     float balance;
 } bank;
+
 void display(bank person);
+
 int main()
 {
     bank person[5];
+
     for (int i = 0; i < 2; i++)
     {
-        printf("Enter the name : ");
+        printf("Enter the name: ");
+        scanf(" %[^\n]", person[i].name); // Read a string with spaces
 
-        gets(person[i].name);
-        fflush(stdin);
-        printf("Enter the accNo : ");
+        printf("Enter the IFSC: ");
+        scanf("%s", person[i].ifsc); // Read IFSC (single word)
 
+        printf("Enter the accNo: ");
         scanf("%d", &person[i].accNo);
 
-        printf("Enter the IFSC : ");
-
-        gets(person[i].ifsc);
-        fflush(stdin);
-        printf("Enter the balance : ");
-
+        printf("Enter the balance: ");
         scanf("%f", &person[i].balance);
     }
-    for (int i = 0; i < 5; i++)
+
+    printf("\nDisplaying Account Details:\n");
+    for (int i = 0; i < 2; i++)
     {
         display(person[i]);
     }
 
     return 0;
 }
+
 void display(bank person)
 {
-    printf("Name = %s\n, A/c No = %d\n,IFSC code = %s\n,Balance = %d\n", person.name, person.accNo, person.balance, person.ifsc);
+    printf("Name = %s\nA/c No = %d\nIFSC code = %s\nBalance = %.2f\n\n",
+           person.name, person.accNo, person.ifsc, person.balance);
 }
