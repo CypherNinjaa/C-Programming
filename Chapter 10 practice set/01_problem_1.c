b78a61cd5b5b13d854560391c57cc9a01595f9a5
vikash <vikashkelly@gmail.com>
2024-12-27 17:02:41 +0530

    problem 1

--- Chapter 10 practice set/01_problem_1.c
+++ Chapter 10 practice set/01_problem_1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main()
 {
+    FILE *ptr;
+    ptr = fopen("integer.txt", "r");
+    // int num = 123;
+    // fprintf(ptr, "%d", num);
+    // fclose;
+
+    int num, num2, num3;
+    fscanf(ptr, "%d %d %d", &num, &num2, &num3);
+    printf("%d %d %d", num, num2, num3);
 
     return 0;
 }
