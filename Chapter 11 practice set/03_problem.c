112567bb0bcf9cc73b318116a386cb549e43777b
vikash <vikashkelly@gmail.com>
2024-12-27 17:03:04 +0530

    problem 3

---
+++ Chapter 11 practice set/03_problem.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n = 6;
+    int *ptr;
+    ptr = (int *)calloc(n, sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the value of array : ");
+        scanf("%d", &ptr[i]);
+    }
+    printf("%c", '\n');
+    for (int i = 0; i < n; i++)
+    {
+        printf("the value of array : ");
+        printf("%d\n", ptr[i]);
+    }
+    free(ptr);
+    return 0;
+}
