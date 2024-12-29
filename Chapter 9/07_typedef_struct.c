efe2b55782ef3d7e6b9f3f6678c31329dc42fd41
vikash <vikashkelly@gmail.com>
2024-12-29 21:16:38 +0530

    revision

--- Chapter 9/07_typedef_struct.c
+++ Chapter 9/07_typedef_struct.c
@@ -12,7 +12,8 @@ int main()
     emp e1;
     emp *ptr = &e1;
     e1.code = 4511;
-    strcpy(e1.name, "harry");
+    printf("Enter the name : ");
+    scanf("%s", &e1.name);
     e1.salary = 54.44;
     printf("%d %f %s\n", e1.code, e1.salary, e1.name);
     printf("%d %f %s", ptr->code, ptr->salary, ptr->name);
