871d81294b03ddb9bdb49b22e3e2129a060b65bb
vikash <vikashkelly@gmail.com>
2025-01-02 12:34:01 +0530

    factorial

---
+++ STET - A/factorial_sum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int fact(int);
+int main()
+{
+    int n, sum = 0;
+    printf("Enter the term : ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum + fact(i);
+    }
+    printf("Sum of factorial = %d", sum);
+    return 0;
+}
+int fact(int n)
+{
+    int facts = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        facts *= i;
+    }
+    return facts;
+}
