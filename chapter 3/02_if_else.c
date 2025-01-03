34b826a66a0c0906e2215c1ddec24a7511496a48
vikash <vikashkelly@gmail.com>
2025-01-03 10:48:36 +0530

    updated

--- chapter 3/02_if_else.c
+++ chapter 3/02_if_else.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int age = 5;
     if (age > 10)
     {
         printf("we are inside if\n");
         printf("your age is greater than 10\n");
     }
-    else{
+    else
+    {
         printf("your age is not greater than 10");
     }
     return 0;
