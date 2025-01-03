3af2cfbe1df77d269f1bcfbfc87e69ab27596961
vikash <vikashkelly@gmail.com>
2025-01-03 10:48:55 +0530

    table

--- File Revision/file_table.c
+++ File Revision/file_table.c
@@ -4,11 +4,11 @@ int main()
     FILE *ptr;
     ptr = fopen("int.txt", "w");
     int table = 4;
+    int table2 = 0;
     for (int i = 1; i <= 10; i++)
     {
-        table = table * i;
-        fprintf(ptr, "%d\n", table);
+        table2 = table * i;
+        fprintf(ptr, "%d\n", table2);
     }
-
     return 0;
 }
